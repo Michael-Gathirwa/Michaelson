38b53fc4b1b8f4c1064aa6ff239891cee4c4f930
Michael-Gathirwa <mikekenn8791@gmail.com>
2023-08-28 01:18:39 +0300

    changes

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function naksVegas(){
 window.location.href="Property Details.html"
 }
+
+gapi.load('auth2', function() {
+    gapi.auth2.init({
+        client_id: 'YOUR_CLIENT_ID',
+    });
+    
+    var signInButton = document.getElementById('google-signin-button');
+    gapi.auth2.getAuthInstance().attachClickHandler(signInButton, {},
+        function(googleUser) {
+            // Handle sign-in success here
+            console.log('Signed in as: ' + googleUser.getBasicProfile().getName());
+        },
+        function(error) {
+            console.error('Error signing in: ' + error);
+        }
+    );
+});
