4079a86d13ab08adbb757502c84dd8f2b8b54a7f
Michael-Gathirwa <135808810+Michael-Gathirwa@users.noreply.github.com>
2023-09-07 20:41:17 +0000

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function naksVegas(){
-window.location.href="Property Details.html"
-}
+    window.location.href="Property Details.html"
+    }
 
 gapi.load('auth2', function() {
     gapi.auth2.init({
